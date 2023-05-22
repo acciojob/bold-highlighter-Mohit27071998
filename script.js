1c6b136674c59e17fdd8a729b86573de64c9b111
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-22 12:46:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+	let ele = document.getElementsByTagName("strong")
+for(let i = 0; i < ele.length; i++){
+	ele[i].style.color = "green";
+}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let ele = document.getElementsByTagName("strong")
+for(let i = 0; i < ele.length; i++){
+	ele[i].style.color = "black";
+}
     
 }
